515d56064a766f7359a6c5547cfcb519a64e5fbb
panditprogrammer <panditprogrammer@gmail.com>
2023-01-06 21:20:35 +0530

    project created

--- djangotutorials/wsgi.py
+++ djangotutorials/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for storefront project.
+WSGI config for djangotutorials project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'storefront.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangotutorials.settings')
 
 application = get_wsgi_application()
