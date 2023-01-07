5dab34422012f7fb8bc3e8ee776fa9e347749d6f
panditprogrammer <panditprogrammer@gmail.com>
2023-01-07 21:44:42 +0530

    create blogs model

---
+++ blogs/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+# Create your models here.
+class Blogs(models.Model):
+    slug = models.SlugField(null=True,blank=True)
+    title = models.CharField(max_length=255)
+    category = models.CharField(max_length=100)
+    description = models.TextField(max_length=255)
+    img = models.ImageField(upload_to="blogs")
+    added_on = models.DateTimeField(auto_now_add=True)
