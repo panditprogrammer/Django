d25e14d5ed3b3998f68890e9103fd13d8032c88b
panditprogrammer <panditprogrammer@gmail.com>
2023-01-07 11:54:01 +0530

    Hello World

--- djangotutorials/urls.py
+++ djangotutorials/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from djangotutorials import views
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/',views.index)
 ]
