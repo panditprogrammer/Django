65286dc2c46dc55fb20615939b0e427892a5149f
panditprogrammer <panditprogrammer@gmail.com>
2023-01-15 17:21:37 +0530

    final setup for vercel

--- djangotutorials/settings.py
+++ djangotutorials/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d8et9z@8kigo#jwv5kcjhxvol01y+uvh(6&q^p*@2z#=zk4vqg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [".vercel.app","now.sh"]
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -132,8 +132,5 @@ STATICFILES_DIRS = [
 ]
 
 # upload image 
-MEDIA_ROOT = BASE_DIR /"media"
+MEDIA_ROOT = BASE_DIR /"media/"
 MEDIA_URL =  "/media/"
-
-# build 
-STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles_build","static")
